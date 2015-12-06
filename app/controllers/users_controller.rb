38893e24d7e9b8c3e86a75a05632448e6e514467
ZHIYUN WU <wzyunzx@163.com>
2015-12-06 21:55:39 +0000

    Add update profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,31 @@ class UsersController < ApplicationController
       end
 
       def create
-     secure_params = params.require(:user).permit(:name, :email, 
+         secure_params = params.require(:user).permit(:name, :email, 
                                   :password, :password_confirmation)
-        @user = User.new(secure_params)
-     if @user.save
-      remember @user
-      flash[:success] = "Welcome to the Twitter App!"
+         @user = User.new(secure_params)
+           if @user.save
+              remember @user
+              flash[:success] = "Welcome to the Twitter App!"
+              redirect_to @user
+            else
+             render 'new' 
+             end
+       end
+      def edit
+         @user = User.find(params[:id])
+      end
+
+
+
+    def update
+      @user = User.find(params[:id])
+     if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
         redirect_to @user
      else
-       render 'new' 
-     end
- end
+      render 'edit'
+    end
+  end
+
 end

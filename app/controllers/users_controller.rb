9ce2bbdecbe7f70b259c0cd9bed4f8869bd09682
ZHIYUN WU <wzyunzx@163.com>
2015-12-06 23:58:11 +0000

    Show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   def show
         @user = User.find(params[:id])
-        
-      
-    end
-
-        
       end
+
+      def index
+        @users = User.all
+  end
 
       def new
       	@user = User.new
@@ -51,6 +50,7 @@ private
    
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
@@ -61,4 +61,4 @@ private
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-    
+end

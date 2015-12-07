0daf714bc73bf29df0b1cd41d7bc90b4ad6184ce
ZHIYUN WU <wzyunzx@163.com>
2015-12-07 11:07:39 +0000

    Add admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206202026) do
+ActiveRecord::Schema.define(version: 20151207104249) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -38,10 +38,11 @@ ActiveRecord::Schema.define(version: 20151206202026) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
